d45d2197e661bfacc372e81a0eff92e769642462
Tylerun_Bot <troberson1997@gmail.com>
2017-12-10 05:06:44 -0600

    Adding Blocks!
    Got blocks working. Textures are still bugged. Looking further into code to find out why.

--- eldercraft/initialization/BlockInitialization.java
+++ eldercraft/initialization/BlockInitialization.java
@@ -1,14 +1,18 @@
 package sambucus.eldercraft.initialization;
 
-import java.awt.List;
+import java.util.List;
 import java.util.ArrayList;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import sambucus.eldercraft.objects.blocks.sugarcube;
 
 
-//public class BlockInitialization {
+public class BlockInitialization {
 
-	//	public static final List<Block> BLOCKS = new ArrayList<Block>();
+		public static final List<Block> BLOCKS = new ArrayList<Block>();
 		
-		//public static final Block Sugar_Cube = new BlockBase("item_PoisonGland"); 
-//}
+		public static final Block SUGARCUBE = new sugarcube("block_sugarcube",Material.GRASS);
+
+		
+}
--- eldercraft/utility/handlers/RegistryHandler.java
+++ eldercraft/utility/handlers/RegistryHandler.java
@@ -1,11 +1,13 @@
 package sambucus.eldercraft.utility.handlers;
 
-import sambucus.eldercraft.initialization.ItemInitialization;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import sambucus.eldercraft.initialization.BlockInitialization;
+import sambucus.eldercraft.initialization.ItemInitialization;
 import sambucus.eldercraft.utility.IHaveModel;
 
 @EventBusSubscriber
@@ -16,6 +18,12 @@ public class RegistryHandler {
 		{
 			event.getRegistry().registerAll(ItemInitialization.ITEMS.toArray(new Item[0]));
 		}
+		
+		@SubscribeEvent
+		public static void onBlockRegister(RegistryEvent.Register<Block> event)
+		{
+			event.getRegistry().registerAll(BlockInitialization.BLOCKS.toArray(new Block[0]));
+		}
 	
 		@SubscribeEvent
 		public static void onModelRegister(ModelRegistryEvent event)
@@ -25,6 +33,13 @@ public class RegistryHandler {
 				if(item instanceof IHaveModel)
 				{
 					((IHaveModel)item).registerModels();
+				}
+			}
+			for(Block block : BlockInitialization.BLOCKS)
+			{
+				if(block instanceof IHaveModel)
+				{
+					((IHaveModel)block).registerModels();
 				}
 			}
 		}
