1f8e46f417a61940033948e19800462f78428a8a
Stratigist <stratigist2@yahoo.com>
2017-12-10 00:32:26 -0500

    running with an issue
    send a woeking set for debug

--- eldercraft/blocks/SugarCube.java
+++ eldercraft/blocks/SugarCube.java
@@ -1,17 +1,26 @@
 package sambucus.eldercraft.blocks;
 
-import sambucus.eldercraft.ElderCraft;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemBlock;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class SugarCube extends Block {
 	public SugarCube() {
 		super(Material.ROCK);
 		setUnlocalizedName("ec0.sugarcube");
 		setRegistryName("sugarcube");
-		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		//setCreativeTab(CreativeTabs.BUILDING_BLOCKS);	
+		//setSoundType();
+		
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void initModel() {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
 	}
 }
