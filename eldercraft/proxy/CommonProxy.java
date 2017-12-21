702077dba43f52b58ee1a2097db34473e713a4a6
Tylerun_Bot <troberson1997@gmail.com>
2017-12-21 00:45:18 -0600

    Food Integration and Move of Smelting Handler
    Added food integration with the meat paddies. Move the smelting handler to its own class.

--- eldercraft/proxy/CommonProxy.java
+++ eldercraft/proxy/CommonProxy.java
@@ -1,20 +1,17 @@
 package sambucus.eldercraft.proxy;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import sambucus.eldercraft.initialization.ItemInitialization;
-import sambucus.eldercraft.utility.handlers.ModRegistryHandler;
+import sambucus.eldercraft.utility.handlers.ModSmeltingHandler;
 
 @Mod.EventBusSubscriber
 public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent e){}
 	public void init(FMLInitializationEvent e){
-		ModRegistryHandler.registerSmelting();
+		ModSmeltingHandler.registerSmelting();
 	}
 	public void postInit(FMLPostInitializationEvent e){} 
 	public void registerItemRenderer(Item item, int meta, String id){}
