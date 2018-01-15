f8e0ed835c4cd7809b71b455463aa5ff3dc9ef3e
Stratigist <stratigist2@yahoo.com>
2018-01-15 03:38:35 -0500

    fixing my derp
    it is 3 am okay?

--- eldercraft/objects/tiles/TileGrinder.java
+++ eldercraft/objects/tiles/TileGrinder.java
@@ -24,10 +24,10 @@ import sambucus.eldercraft.initialization.ItemInitialization;
 import sambucus.eldercraft.utility.managers.GrinderManager;
 
 public class TileGrinder extends TileEntity implements IInventory, ITickable {
-	//part of the fuel storage
+	/*/TODO part of the fuel storage
 	private final int FUEL_MAX = 1000;//may be a config thing later
 	private int intFuelHave = 0;//FUEL IN THE TANK
-	
+	*/
 	private ItemStack[] itemStacks;
 	private boolean cachedBurn = false;
 	public static final int OUTPUT_SLOTS_COUNT = 4;
@@ -37,10 +37,10 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 	public static final int GRIND_SLOT = 1;
 	public static final int CAN_SLOT = 2;
 	public static final int FIRST_OUTPUT_SLOT = 3;
-	private int burnTimeRemaining = 0;
-	private int burnTimeInitialValue;
+	private int burnTimeRemaining = 0;//The number of burn ticks remaining on the current piece of fuel
+	private int burnTimeInitialValue;//The initial fuel value of the currently burning fuel (in ticks of burn duration)
 	private short grindTime;
-	//private static final short GRIND_TIME_FOR_COMPLETION = 200; // time is not the same for all items
+	private static final short GRIND_TIME_FOR_COMPLETION = 200; //TODO time is not the same for all items need fix
 	private static final byte GRIND_FIELD_ID = 0;
 	private static final byte BURN_TIME_REMAINING_FIELD_ID = 1;
 	private static final byte BURN_TIME_INITIAL_FIELD_ID = 2;
@@ -54,29 +54,24 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 	//active area
 	@Override
 	public void update() {
+		// TODO the thing that is updated each tick	
 		//add fuel to "tank"
-		
-		if (intFuelHave < FUEL_MAX) {
-			boolean fuelBurning = burnFuel();
-		}
 		if (canGrind()) {//change this to if there is room in the tank
-			
+			boolean fuelBurning = burnFuel();
 			// If fuel is available, keep cooking the item, otherwise start "uncooking" it at double speed
-			if (intFuelHave > 0) {
+			if (fuelBurning) {//TODO replace this with fuel tank info
 				grindTime++;//will need to change this to adding to tank
-				--intFuelHave;
 			}else{
 				grindTime -= 2;
 			}
 			if (grindTime < 0) {
 				grindTime = 0;
 			}
-			if (grindTime >= getGrindCost(itemStacks[GRIND_SLOT])) { //GRIND_TIME_FOR_COMPLETION
+			if (grindTime >= GRIND_TIME_FOR_COMPLETION) {
 				grindItem();
 				grindTime = 0;
 			}
-		}
-		else{
+		}else{
 			grindTime = 0;
 		}
 		
@@ -100,7 +95,6 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 		boolean inventoryChanged = false;
 		if (burnTimeRemaining > 0) {
 			--burnTimeRemaining;
-			intFuelHave++;
 			burning = true;
 		}
 		if (burnTimeRemaining == 0) {
@@ -126,221 +120,86 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 		grindItem(true);
 	}
 	private boolean grindItem(boolean performGrind) {
+		//this is called every tick
+		//TODO have all the outputs show up.
 		Integer suitableInputSlot = null;
-		Integer validOutputSlot1 = null;
-		Integer validOutputSlot2 = null;
-		Integer validOutputSlot3 = null;
-		Integer volume1 = 0;
-		Integer volume2 = 0;
-		Integer volume3 = 0;
-		Integer volume1True = 0;
-		Integer volume2True = 0;
-		Integer volume3True = 0;
-		ItemStack result1 = ItemStack.EMPTY;
+		Integer firstSuitableOutputSlot = null;
+		Integer volume1 = null;
+		Integer volume2 = null;
+		Integer volume3 = null;
+		ItemStack result1 = ItemStack.EMPTY;  //EMPTY_ITEM
 		ItemStack result2 = ItemStack.EMPTY;
 		ItemStack result3 = ItemStack.EMPTY;
-		Boolean canCheck = false;
-		Boolean out2Flag = false;
-		Boolean out3Flag = false;
-		
-		
-		
 		if (!itemStacks[GRIND_SLOT].isEmpty()) {
 			result1 = getGrindingResultR1(itemStacks[GRIND_SLOT]);
 			result2 = getGrindingResultR2(itemStacks[GRIND_SLOT]);
 			result3 = getGrindingResultR3(itemStacks[GRIND_SLOT]);
-			volume1 = getGrindingResultV1(itemStacks[GRIND_SLOT]);
-			volume2 = getGrindingResultV2(itemStacks[GRIND_SLOT]);
-			volume3 = getGrindingResultV3(itemStacks[GRIND_SLOT]);
-			canCheck = GrinderManager.instance().getCanCheck(itemStacks[GRIND_SLOT]);
-			
+			//TODO may need this later --- System.err.println("there is an item in the grind slot " + itemStacks[GRIND_SLOT].getDisplayName() + " and it will make " + result.getDisplayName());
 			if (!result1.isEmpty()) {
-				if(canCheck && itemStacks[CAN_SLOT].isEmpty()) {
-					return false;
-				}
-				Integer volume1Base  = (volume1 < 100) ? 0 : (Integer) volume1 / 100;
-				Integer volume1Chance = (volume1 == 100) ? 0 : volume1 % 100;
-				volume1True = ((Math.random() * 100) < volume1Chance) ? (volume1Base + 1) : (volume1Base);
-				if(!result2.isEmpty()) {
-					Integer volume2Base  = (volume2 < 100) ?  0 : (Integer) volume2 / 100;
-					Integer volume2Chance = (volume2 == 100) ? 0 : volume2 % 100;
-					volume2True = ((Math.random() * 100) < volume2Chance) ? (volume2Base + 1) : (volume2Base);
-				}
-				else {
-					out2Flag = false;
-				}
-				if(!result3.isEmpty()) {
-					Integer volume3Base  = (volume3 < 100) ?  0 : (Integer) volume3 / 100;
-					Integer volume3Chance = (volume3 == 100) ? 0 : volume3 % 100;
-					volume3True = ((Math.random() * 100) < volume3Chance) ? (volume3Base + 1) : (volume3Base);
-				}
-				else {
-					out3Flag = false;
-				}
-				for (int outputSlot1 = FIRST_OUTPUT_SLOT; outputSlot1 < FIRST_OUTPUT_SLOT + OUTPUT_SLOTS_COUNT; outputSlot1++) {
-					ItemStack outputStack1 = itemStacks[outputSlot1];// 3 4 5 6
-					
-					if (outputStack1.isEmpty()) {
+				for (int outputSlot = FIRST_OUTPUT_SLOT; outputSlot < FIRST_OUTPUT_SLOT + OUTPUT_SLOTS_COUNT; outputSlot++) {
+					ItemStack outputStack = itemStacks[outputSlot];
+					if (outputStack.isEmpty()) {
 						suitableInputSlot = GRIND_SLOT;
-						validOutputSlot1 = outputSlot1;
+						firstSuitableOutputSlot = outputSlot;
 						break;
 					}
-					if (outputStack1.getItem() == result1.getItem() &&
-							(!outputStack1.getHasSubtypes() || outputStack1.getMetadata() == outputStack1.getMetadata())
-							&& ItemStack.areItemStackTagsEqual(outputStack1, result1)) {
+					if (outputStack.getItem() == result1.getItem() &&
+							(!outputStack.getHasSubtypes() ||
+									outputStack.getMetadata() == outputStack.getMetadata())
+							&& ItemStack.areItemStackTagsEqual(outputStack, result1)) {
 						
-						int combinedSize = itemStacks[outputSlot1].getCount() + volume1True;
+						int combinedSize = itemStacks[outputSlot].getCount() + result1.getCount();
 						
-						if (combinedSize <= getInventoryStackLimit() && combinedSize <= itemStacks[outputSlot1].getMaxStackSize()) {
+						
+						if (combinedSize <= getInventoryStackLimit() && combinedSize <= itemStacks[outputSlot].getMaxStackSize()) {
 							suitableInputSlot = GRIND_SLOT;
-							validOutputSlot1 = outputSlot1;
+							firstSuitableOutputSlot = outputSlot;
 							break;
 						}
 					}
-				}
-				//item 2
-				if(!result2.isEmpty() && volume2True > 0) {
-					out2Flag = true;
-					for (int outputSlot2 = FIRST_OUTPUT_SLOT; outputSlot2 < FIRST_OUTPUT_SLOT + OUTPUT_SLOTS_COUNT; outputSlot2++) {
-						ItemStack outputStack2 = itemStacks[outputSlot2];// 3 4 5 6
-						
-						if (outputStack2.isEmpty() && validOutputSlot1 != outputSlot2) {
-							validOutputSlot2 = outputSlot2;
-							break;
-						}
-						if (outputStack2.getItem() == result2.getItem() &&
-								(!outputStack2.getHasSubtypes() || outputStack2.getMetadata() == outputStack2.getMetadata())
-								&& ItemStack.areItemStackTagsEqual(outputStack2, result2)) {
-							
-							int combinedSize = itemStacks[outputSlot2].getCount() + volume2True;
-						
-							if (combinedSize <= getInventoryStackLimit() && combinedSize <= itemStacks[outputSlot2].getMaxStackSize()) {
-								validOutputSlot2 = outputSlot2;
-								break;
-							}
-						}
-					}
-					//item 3
-					if(!result3.isEmpty() && volume3True > 0) {
-						out3Flag = true;
-						for (int outputSlot3 = FIRST_OUTPUT_SLOT; outputSlot3 < FIRST_OUTPUT_SLOT + OUTPUT_SLOTS_COUNT; outputSlot3++) {
-							ItemStack outputStack3 = itemStacks[outputSlot3];// 3 4 5 6
-					
-							if (outputStack3.isEmpty() && (validOutputSlot1 != outputSlot3) && (validOutputSlot2 != outputSlot3)) {
-								validOutputSlot3 = outputSlot3;
-								break;
-							}
-							if (outputStack3.getItem() == result3.getItem() &&
-									(!outputStack3.getHasSubtypes() || outputStack3.getMetadata() == outputStack3.getMetadata())
-									&& ItemStack.areItemStackTagsEqual(outputStack3, result3)) {
-						
-								int combinedSize = itemStacks[outputSlot3].getCount() + volume3True;
-								
-								if (combinedSize <= getInventoryStackLimit() && combinedSize <= itemStacks[outputSlot3].getMaxStackSize()) {
-									validOutputSlot3 = outputSlot3;
-									break;
-								}
-							}
-						}
-					}
-					else {
-						out3Flag = false;
-					}
-				}
-				else{
-					out2Flag = false;
-				}
+				}	
 			}
 		}
-		if (suitableInputSlot == null || validOutputSlot1 == null ||
-				(out2Flag == true && validOutputSlot2 == null) ||
-				(out3Flag == true && validOutputSlot3 == null)){
-			
+		if (suitableInputSlot == null) {
 			return false;
 		}
-		
-		if (!performGrind){
-			
+		if (!performGrind) {
 			return true;
 		}
-		//TODO
-		System.err.println("grind info " + result1.getDisplayName() + " " + volume1True + " from " + volume1);
-		System.err.println(result2.getDisplayName() + " " + volume2True + " from " + volume2);
-		System.err.println(result3.getDisplayName() + " " + volume3True + " from " + volume3);
-		System.err.println("random " + Math.random());
+		
 		// alter input and output
 		itemStacks[suitableInputSlot].shrink(1);
-		if (itemStacks[suitableInputSlot].getCount() <= 0){
+		if (itemStacks[suitableInputSlot].getCount() <= 0) {
 			itemStacks[suitableInputSlot] = ItemStack.EMPTY;
 		}
-		if(canCheck) {
-			itemStacks[CAN_SLOT].shrink(1);
-			if(itemStacks[CAN_SLOT].getCount() <= 0) {
-				itemStacks[CAN_SLOT] = ItemStack.EMPTY;
-			}
-		}
-		
-		if (itemStacks[validOutputSlot1].isEmpty()){
-			ItemStack result1Modified = result1.copy();
-			
-			result1Modified.setCount(volume1True);
-			itemStacks[validOutputSlot1] = result1Modified.copy();
-		}
-		else{
-			int newStackSize = itemStacks[validOutputSlot1].getCount() + volume1True;
-			itemStacks[validOutputSlot1].setCount(newStackSize);
-		}
-		//item2
-		if(out2Flag) {
-			if (itemStacks[validOutputSlot2].isEmpty()){
-				ItemStack result2Modified = result2.copy();
-				
-				result2Modified.setCount(volume2True);
-				itemStacks[validOutputSlot2] = result2Modified.copy();
-			}
-			else{
-				int newStackSize = itemStacks[validOutputSlot2].getCount() + volume2True;
-				itemStacks[validOutputSlot2].setCount(newStackSize);
-			}
-		}
-		//item3
-		if(out3Flag) {
-			if (itemStacks[validOutputSlot3].isEmpty()){
-				ItemStack result3Modified = result3.copy();
-				
-				result3Modified.setCount(volume2True);
-				itemStacks[validOutputSlot2] = result3Modified.copy();
-			}
-			else{
-				int newStackSize = itemStacks[validOutputSlot3].getCount() + volume3True;
-				itemStacks[validOutputSlot2].setCount(newStackSize);
-			}
+		// check if a can is used and shrink that also
+		if (itemStacks[firstSuitableOutputSlot].isEmpty()) {
+			itemStacks[firstSuitableOutputSlot] = result1.copy();
+		}else{
+			int newStackSize = itemStacks[firstSuitableOutputSlot].getCount() + result1.getCount();
+			itemStacks[firstSuitableOutputSlot].setCount(newStackSize);
 		}
 		markDirty();
 		return true;
 	}
-	
 	//output block
-	public static ItemStack getGrindingResultR1(ItemStack stack){
+	public static ItemStack getGrindingResultR1(ItemStack stack) {
 		return GrinderManager.instance().getGrindingR1(stack);
 	}
-	public static int getGrindingResultV1(ItemStack stack){
+	public static int getGrindingResultV1(ItemStack stack) {
 		return GrinderManager.instance().getGrindingV1(stack);
 	}
-	public static ItemStack getGrindingResultR2(ItemStack stack){
+	public static ItemStack getGrindingResultR2(ItemStack stack) {
 		return GrinderManager.instance().getGrindingR2(stack);
 	}
-	public static int getGrindingResultV2(ItemStack stack){
+	public static int getGrindingResultV2(ItemStack stack) {
 		return GrinderManager.instance().getGrindingV2(stack);
 	}
-	public static ItemStack getGrindingResultR3(ItemStack stack){
+	public static ItemStack getGrindingResultR3(ItemStack stack) {
 		return GrinderManager.instance().getGrindingR3(stack);
 	}
-	public static int getGrindingResultV3(ItemStack stack){
+	public static int getGrindingResultV3(ItemStack stack) {
 		return GrinderManager.instance().getGrindingV3(stack);
-	}
-	public static int getGrindCost(ItemStack stack) {
-		return GrinderManager.instance().getGrindingCost(stack);
 	}
 	//NBT area
 	@Override
@@ -360,15 +219,17 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 		
 		NBTTagList dataForAllSlots = new NBTTagList();
 		for (int i = 0; i < this.itemStacks.length; ++i) {
-			if (!this.itemStacks[i].isEmpty()){
+			if (!this.itemStacks[i].isEmpty()) {  //isEmpty()
 				NBTTagCompound dataForThisSlot = new NBTTagCompound();
 				dataForThisSlot.setByte("Slot", (byte) i);
 				this.itemStacks[i].writeToNBT(dataForThisSlot);
 				dataForAllSlots.appendTag(dataForThisSlot);
 			}
 		}
+		
 		// the array of hashmaps is then inserted into the parent hashmap for the container
 		parentNBTTagCompound.setTag("Items", dataForAllSlots);
+
 		// Save everything else
 		parentNBTTagCompound.setShort("GrindTime", grindTime);
 		parentNBTTagCompound.setTag("burnTimeRemaining", new NBTTagInt(burnTimeRemaining));
@@ -379,6 +240,7 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 	@Override
 	public void readFromNBT(NBTTagCompound parentNBTTagCompound){
 		// TODO whatever the fuck needs to be added to make this work for us ... i can't give a shit much longer
+		
 		super.readFromNBT(parentNBTTagCompound); // The super call is required to save and load the tiles location
 		final byte NBT_TYPE_COMPOUND = 10;       // See NBTBase.createNewByType() for a listing
 		NBTTagList dataForAllSlots = parentNBTTagCompound.getTagList("Items", NBT_TYPE_COMPOUND);
@@ -425,7 +287,8 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 		return burnTimeRemaining / 20; // 20 ticks per second
 	}
 	public double fractionOfGrindTimeComplete(){
-		double fraction = grindTime / (double)getGrindCost(itemStacks[GRIND_SLOT]);
+		//TODO part of the variable time rework
+		double fraction = grindTime / (double)GRIND_TIME_FOR_COMPLETION;
 		return MathHelper.clamp(fraction, 0.0, 1.0);
 	}
 	
@@ -521,10 +384,10 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 			return grindTime;
 		}
 		if (id == BURN_TIME_REMAINING_FIELD_ID ) {
-			return burnTimeRemaining;
+			//return burnTimeRemaining;
 		}
 		if (id == BURN_TIME_INITIAL_FIELD_ID) {
-			return burnTimeInitialValue;
+			//return burnTimeInitialValue;
 		}
 		//TODO this needs to be fixed
 		//System.err.println("the ID in getField is wrong " + id + " is not accounted for");
@@ -532,6 +395,7 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 	}
 	@Override
 	public void setField(int id, int value) {
+		// TODO this don't look right...copy pasta code
 		if (id == GRIND_FIELD_ID) {
 			grindTime = (short)value;
 		} else if (id == BURN_TIME_REMAINING_FIELD_ID ) {
@@ -560,13 +424,13 @@ public class TileGrinder extends TileEntity implements IInventory, ITickable {
 		return true;
 	}
 	public static boolean isItemValidForCanSlot(ItemStack stack) {
-		if(stack.areItemsEqualIgnoreDurability(stack, new ItemStack(ItemInitialization.IRONCAN)) ) {
-			return true;//later we will check for other types of cans
+		if(stack.areItemsEqualIgnoreDurability(stack, new ItemStack(ItemInitialization.IRONCAN)) ) {//later we will check for other types of cans
+			return true;
 		}
 		return false;
 	}
 	public static short getItemBurnTime(ItemStack stack){//rather than use it as time use a ++fuel  and  --fuel when doing a task
-		int burntime = TileEntityFurnace.getItemBurnTime(stack);
+		int burntime = TileEntityFurnace.getItemBurnTime(stack);  // just use the vanilla values
 		return (short)MathHelper.clamp(burntime, 0, Short.MAX_VALUE);
 	}
 	@Override
--- eldercraft/utility/managers/GrinderManager.java
+++ eldercraft/utility/managers/GrinderManager.java
@@ -2,8 +2,6 @@ package sambucus.eldercraft.utility.managers;
 
 import gnu.trove.map.hash.THashMap;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFishFood;
 import net.minecraft.item.ItemStack;
 import sambucus.eldercraft.initialization.ItemInitialization;
 
@@ -53,79 +51,57 @@ public class GrinderManager {
 	public static GrinderManager instance() {
 		return GRINDING_BASE;
 	}
-	private GrinderManager(){
+	private GrinderManager() {
 		//the place the list is to be written
-		this.addGinderRecipe(Items.BEEF, 400, false,
+		this.addGinderRecipe(new ItemStack(Items.BEEF), 400, false,
 				new ItemStack(ItemInitialization.PATTYRAWBEEF), 350,
-				new ItemStack(Items.DYE , 1, 15), 175,
+				new ItemStack(Items.DYE , 15), 175,
 				new ItemStack(Items.LEATHER), 100);
 		
-		this.addGinderRecipe(Items.PORKCHOP, 200, false,
+		this.addGinderRecipe(new ItemStack(Items.PORKCHOP), 200, false,
 				new ItemStack(ItemInitialization.PATTYRAWPORK), 225,
-				new ItemStack(Items.DYE , 1, 15), 60,
+				new ItemStack(Items.DYE , 15), 60,
 				new ItemStack(Items.LEATHER), 30);
 		
-		this.addGinderRecipe(Items.CHICKEN, 300, false,
+		this.addGinderRecipe(new ItemStack(Items.CHICKEN), 300, false,
 				new ItemStack(ItemInitialization.PATTYRAWCHICKEN), 100,
-				new ItemStack(Items.DYE , 1, 15), 80,
+				new ItemStack(Items.DYE , 15), 80,
 				new ItemStack(Items.WHEAT_SEEDS), 100);//change it to random seed type later
 		
-		this.addGinderRecipe(Items.RABBIT, 100, false,
+		this.addGinderRecipe(new ItemStack(Items.RABBIT), 100, false,
 				new ItemStack(ItemInitialization.PATTYRAWLEAN), 100,
-				new ItemStack(Items.DYE , 1, 15), 10,
+				new ItemStack(Items.DYE , 15), 10,
 				new ItemStack(Items.RABBIT_FOOT), 1);
 		
-		this.addGinderRecipe(Items.MUTTON, 200, false,
-				new ItemStack(ItemInitialization.PATTYRAWMUTTON), 175,
-				new ItemStack(Items.DYE, 1, 15), 30,
-				ItemStack.EMPTY, 0);
-		
-		this.addGinderRecipeFinal(new ItemStack(Items.FISH , 1, 0), 100, false,//normal
+		this.addGinderRecipe(new ItemStack(Items.FISH , 0), 100, false,//normal
 				new ItemStack(ItemInitialization.PATTYRAWFISH), 100,
-				new ItemStack(Items.DYE ,1, 15), 50,
+				new ItemStack(Items.DYE , 15), 50,
 				new ItemStack(ItemInitialization.FISHSCALES), 10);
 		
-		this.addGinderRecipeFinal(new ItemStack(Items.FISH , 1, 1), 200, false,//salmon
+		this.addGinderRecipe(new ItemStack(Items.FISH , 1), 200, false,//salmon
 				new ItemStack(ItemInitialization.PATTYRAWFISH), 125,
-				new ItemStack(Items.DYE , 1, 15), 80,
+				new ItemStack(Items.DYE , 15), 80,
 				new ItemStack(ItemInitialization.FISHSCALES), 80);
 		
-		this.addGinderRecipeFinal(new ItemStack(Items.FISH , 1, 2), 100, false,//clown
+		this.addGinderRecipe(new ItemStack(Items.FISH , 2), 100, false,//clown
 				new ItemStack(ItemInitialization.PATTYRAWFISH), 75,
-				new ItemStack(Items.DYE , 1, 15), 25,
+				new ItemStack(Items.DYE , 15), 25,
 				new ItemStack(ItemInitialization.OILYSKIN), 75);
 		
-		this.addGinderRecipeFinal(new ItemStack(Items.FISH , 1, 3), 100, false,//puff
+		this.addGinderRecipe(new ItemStack(Items.FISH , 3), 100, false,//puff
 				new ItemStack(ItemInitialization.PATTYRAWFISH), 80,
-				new ItemStack(Items.DYE , 1, 15), 10,
+				new ItemStack(Items.DYE , 15), 10,
 				new ItemStack(ItemInitialization.POISONGLAND), 100);
-		
-		this.addGinderRecipe(ItemInitialization.CLUSTEROFBERRIES, 100, true,//test
-				new ItemStack(ItemInitialization.MASHGROWING), 100,
-				new ItemStack(ItemInitialization.JUICEBURNTCAN), 100,
-				ItemStack.EMPTY, 0);
 	}
-	public void addGinderRecipe(Item input, int fuel, boolean hasCan,
-			ItemStack output1, int output1Volume,
-			ItemStack output2, int output2Volume,
-			ItemStack output3, int output3Volume) {
-		this.addGinderRecipeFinal(new ItemStack(input, 1, 32767), fuel, hasCan,
-				output1, output1Volume,
-				output2, output2Volume,
-				output3, output3Volume);
-	}
-	
-	public void addGinderRecipeFinal(
+	public void addGinderRecipe(
 			ItemStack input, int fuel, boolean hasCan,
 			ItemStack output1, int output1Volume,
 			ItemStack output2, int output2Volume,
-			ItemStack output3, int output3Volume){
-		
+			ItemStack output3, int output3Volume) {
 		if (getGrindingR1(input) != ItemStack.EMPTY){
 			net.minecraftforge.fml.common.FMLLog.log.info("Ignored grinding recipe with conflicting input: {} = {}", input, output1);
 			return;
 		}
-		
 		this.grindingR1List.put(input, output1);
 		this.grindingR2List.put(input, output2);
 		this.grindingR3List.put(input, output3);
